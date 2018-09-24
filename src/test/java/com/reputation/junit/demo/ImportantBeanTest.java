b11c8ff6013d06ee4140ec770782760b1f7d8132
pmorah <pmora@simpartners.com>
2018-09-23 21:20:48 -0300

    Initial commit

--- src/test/java/com/reputation/junit/demo/ImportantBeanTest.java
+++ src/test/java/com/reputation/junit/demo/ImportantBeanTest.java
@@ -1,6 +1,5 @@
 package com.reputation.junit.demo;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
