126ebeaee878f6cbf949dfacf827d864e1a864be
pmorah <pmora@simpartners.com>
2018-10-02 12:07:35 -0300

    added dockerfile

--- src/test/java/com/reputation/junit/demo/controller/ImportantControllerIT.java
+++ src/test/java/com/reputation/junit/demo/controller/ImportantControllerIT.java
@@ -5,10 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
@@ -17,30 +14,72 @@ import static org.junit.Assert.assertTrue;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ImportantControllerIT {
-    TestRestTemplate restTemplate;
+    private TestRestTemplate restTemplate;
+    private String serviceUrl;
 
     @Before
     public void setUp() {
         restTemplate = new TestRestTemplate();
+        serviceUrl = "http://localhost:8090";
     }
 
     @Test
     public void getIndexTest1() {
         HttpEntity<String> entity = new HttpEntity<>(null, new HttpHeaders());
 
-        System.out.println("Body1... ");
-        String response = restTemplate.getForObject("http://localhost:8090/api/", String.class);
-
-        System.out.println("Body2... ");
-        System.out.println("Body: " + response);
+        String response = restTemplate.getForObject(this.serviceUrl, String.class);
 
         assertEquals("Hello World!", response);
-
-        //assertTrue("getIndexTest",2 == 2);
     }
 
     @Test
     public void getIndexTest2() {
         assertTrue("getIndexTest",3 == 3);
+    }
+
+    @Test
+    public void getIndexTest3() {
+        String response = restTemplate.getForObject(this.serviceUrl, String.class);
+
+        assertEquals("Hello World!", response);
+    }
+
+    @Test
+    public void getIndexTest4() {
+        String response = restTemplate.getForObject(this.serviceUrl, String.class);
+
+        assertEquals("Hello World!", response);
+    }
+
+
+    @Test
+    public void getIndexTest5() {
+        String response = restTemplate.getForObject(this.serviceUrl, String.class);
+
+        assertEquals("Hello World!", response);
+    }
+
+    @Test
+    public void getIndexTest6() {
+        String response = restTemplate.getForObject(this.serviceUrl, String.class);
+
+        assertEquals("Hello World!", response);
+    }
+
+    @Test
+    public void getIndexTest7() {
+        String response = restTemplate.getForObject(this.serviceUrl, String.class);
+
+        assertEquals("Hello World!", response);
+    }
+
+    @Test
+    public void getIndexTest8() {
+        HttpEntity<String> entity = new HttpEntity<>(null, new HttpHeaders());
+
+        ResponseEntity<String> response = restTemplate.exchange(this.serviceUrl, HttpMethod.GET, entity, String.class);
+
+        System.out.println("Response Code: " + response.getStatusCode());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
