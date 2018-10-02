c1d54f7fde5614c0ad80b54c66bbd5ef0341a33a
pmorah <pmora@simpartners.com>
2018-10-02 10:00:29 -0300

    added dockerfile

---
+++ src/test/java/com/reputation/junit/demo/controller/ImportantControllerIT.java
@@ -0,0 +1,20 @@
+package com.reputation.junit.demo.controller;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImportantControllerIT {
+
+    @Test
+    public void getIndexTest() {
+        assertTrue("getIndexTest",2 == 2);
+    }
+}
